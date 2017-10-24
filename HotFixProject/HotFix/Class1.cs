39adf1adc6b55278115f8773fa4c6c6ff4d59c21
wana_chengwang <chengwang2000>
2017-10-24 18:51:38 +0800

    Use type.fullname now

--- HotFixProject/HotFix/Class1.cs
+++ HotFixProject/HotFix/Class1.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using UnityEngine;
 
-namespace HotFix {
+// Patch the same fullname(namespace&classname)
+namespace ILHotFix {
     public class ILHFHelloWorld {
         public static void Start(ILHFHelloWorld self) {
-            Debug.Log("HotFix.ILHFHelloWorld.Start.");
+            Debug.Log("HotFix:ILHFHelloWorld.Start.");
         }
         static int _cnt = 0;
         public static void Update(ILHFHelloWorld self) {
             if((_cnt++&0x3f) == 0) {
-                Debug.Log("HotFix.ILHFHelloWorld.Update.");
+                Debug.Log("HotFix:ILHFHelloWorld.Update.");
             }
         }
     }
