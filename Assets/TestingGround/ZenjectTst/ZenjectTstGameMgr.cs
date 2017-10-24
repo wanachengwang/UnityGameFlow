cf5885898e8499923a2c484d2951e483eb080251
wana_chengwang <chengwang2000>
2017-10-24 18:53:58 +0800

    Add ZenjectTst code and project

---
+++ Assets/TestingGround/ZenjectTst/ZenjectTstGameMgr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class ZenjectTstGameMgr : ITickable {
+    static int _n = 0;
+    public ZenjectTstGameMgr() {
+        _n++;
+        Debug.Log("ZenjectTstGameMgr:"+_n);
+    }
+
+    void ITickable.Tick() {
+        if (Input.GetKey(KeyCode.A)) {
+            Debug.Log("A Pressed!!!!!!");
+        }
+    }
+}
---
+++ Assets/TestingGround/ZenjectTst/ZenjectTstSceneInstaller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class ZenjectTstSceneInstaller : MonoInstaller<ZenjectTstSceneInstaller> {
+    public override void InstallBindings() {
+        Container.BindInterfacesTo<ZenjectTstGameMgr>().AsTransient();
+    }
+}
